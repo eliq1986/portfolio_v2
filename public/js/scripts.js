7a41d37daace344731d5713d7d42e097946fec93
Elijah Quesada <this_boy10@yahoo.com>
2019-09-13 06:05:46 -0700

    Placed setTimeout calls into a func. Removing duplication code.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,12 +4,60 @@ $(document).foundation();
 $( document ).ready(function() {
 
 
+const colors = [,, , ];
+
+const buttons = [
+  {
+    color:"#0077B5",
+    startTime: 500,
+    endTime: 900
+  },
+{
+    color: "#1DA1F2",
+    startTime: 800,
+    endTime: 1150,
+  },
+  {
+    color:"#c9510c",
+    startTime:1050,
+    endTime: 1400,
+  },
+  {
+    color: "#5fcf80",
+    startTime: 1300,
+    endTime: 1650
+  }
+]
+
   const portfolioIndex = $(".portfolio-index");
   const aboutMe = $(".fadeIn");
 
   aboutMe.fadeIn(500);
   portfolioIndex.fadeIn(500);
 
+ const buttonExpanded = $(".button");
 
+
+ buttonExpanded.each(function(index, value) {
+   const { color, startTime, endTime } = buttons[index];
+   changeColors( startTime, endTime, color, index);
+ });
+
+ function changeColors(startTime, stopTime, color, buttonIndex) {
+
+   setTimeout(function() {
+       buttonExpanded.eq(buttonIndex).css({background: color});
+
+   }, startTime);
+
+   setTimeout(function() {
+       buttonExpanded.eq(buttonIndex).css("background", "#333");
+   }, stopTime)
+ }
+
+
+buttonExpanded.eq(0).hover(function() {
+  this.style.background = "#0077B5";
+});
 
 });
